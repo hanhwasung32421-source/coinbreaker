fe1ddabe7a6d36a20a1d8a49071169e2e75079d6
hwasung <hanhwasung32421@gmail.com>
2026-06-08 15:28:16 +0900

    update

--- deploy.bat
+++ deploy.bat
@@ -2,12 +2,18 @@
 cd /d C:\trae\coinbreaker
 if exist ".git\index.lock" del /f ".git\index.lock"
 git remote set-url origin https://github.com/hanhwasung32421-source/coinbreaker.git
-git branch -M main
+
+for /f %%b in ('git rev-parse --abbrev-ref HEAD') do set BRANCH=%%b
+if "%BRANCH%"=="" (
+  echo failed to detect current branch
+  exit /b 1
+)
+
 git add -A
 git diff --cached --quiet
 if errorlevel 1 (
   git commit -m "update"
-  git push -u origin main
+  git push -u origin %BRANCH%
 ) else (
   echo no changes to commit
 )
